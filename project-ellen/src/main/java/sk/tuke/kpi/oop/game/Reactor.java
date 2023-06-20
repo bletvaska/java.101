4782e8076041a97bb2132cb37bf6423abd724d6b
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-06-20 12:47:09 +0200

    funkcia increaseTemperature() na pokazenie reaktora

--- project-ellen/src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ project-ellen/src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -12,17 +12,17 @@ public class Reactor extends AbstractActor {
     private Animation normalAnimation;
 
     public Reactor() {
-        temperature = 0;
-        state = false;
-        damage = 0;
-        normalAnimation = new Animation(
+        this.temperature = 0;
+        this.state = false;
+        this.damage = 0;
+        this.normalAnimation = new Animation(
             "sprites/reactor_on.png",
             80, 80,
             0.1F,
             Animation.PlayMode.LOOP_PINGPONG
         );
 
-        setAnimation(normalAnimation);
+        setAnimation(this.normalAnimation);
     }
 
     public int getTemperature() {
@@ -30,12 +30,20 @@ public class Reactor extends AbstractActor {
     }
 
     public int getDamage() {
-        return damage;
+        return this.damage;
     }
 
     public void increaseTemperature(int increment){
+        this.temperature = this.temperature + increment;
 
+        // when temperature is above 2000, then damage increases
+        if(this.temperature >= 2000 && this.temperature <= 6000) {
+            int damage = this.temperature / 40 - 50;
+
+            // update when current damage is bigger than previous
+            if(this.damage < damage){
+                this.damage = damage;
+            }
+        }
     }
-
-
 }
