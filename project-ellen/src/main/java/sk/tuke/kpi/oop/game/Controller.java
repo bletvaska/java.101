d0d19912d8e532d066d1915af9dfa5cf4f692472
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-06-22 11:01:41 +0200

    v kontroleri vytvoreny parametricky konstruktor

--- project-ellen/src/main/java/sk/tuke/kpi/oop/game/Controller.java
+++ project-ellen/src/main/java/sk/tuke/kpi/oop/game/Controller.java
@@ -4,8 +4,10 @@ import sk.tuke.kpi.gamelib.framework.AbstractActor;
 import sk.tuke.kpi.gamelib.graphics.Animation;
 
 public class Controller extends AbstractActor {
+    private Reactor reactor;
 
-    public Controller(){
+    public Controller(Reactor reactor){
+        this.reactor = reactor;
         Animation animation = new Animation("sprites/switch.png");
         setAnimation(animation);
     }
