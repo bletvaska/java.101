a0b937e5ee8c8e876598a614766d495effb3ce5e
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-06-22 08:26:56 +0200

    vytvoreny pocitac (len blikajuci)

---
+++ project-ellen/src/main/java/sk/tuke/kpi/oop/game/Computer.java
@@ -0,0 +1,16 @@
+package sk.tuke.kpi.oop.game;
+
+import sk.tuke.kpi.gamelib.framework.AbstractActor;
+import sk.tuke.kpi.gamelib.graphics.Animation;
+
+public class Computer extends AbstractActor {
+    public Computer() {
+        Animation animation = new Animation(
+            "sprites/computer.png",
+            80, 48,
+            0.1f,
+            Animation.PlayMode.LOOP_PINGPONG
+        );
+        setAnimation(animation);
+    }
+}
